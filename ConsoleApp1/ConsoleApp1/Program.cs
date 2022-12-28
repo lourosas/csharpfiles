e7b3368e7a28c8581ad536fc0e7a352801858c39
Lou Rosas <lourosas@gmail.com>
2022-12-28 12:56:51 -0700

    12-28-22-00

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   class Program
+   {
+      /*Poop and more poop*/
+      static void Main(string[] args)
+      {
+         Console.WriteLine(args.Length);
+         Console.WriteLine("Hello World");
+         var data = 1234;
+         Console.WriteLine(data);
+         new Program();
+      }
+
+      public Program()
+      {
+         Console.WriteLine("Starting Program");
+         Console.WriteLine("================");
+         this.SetUpA();
+         this.SetUpB();
+         Console.WriteLine("================");
+         Console.WriteLine("The End");
+      }
+
+      private void SetUpA() {
+         Console.WriteLine("SetUpA()");
+         AClass a = new AClass();
+         Console.WriteLine(a.ToString);
+         AClass a1 = new AClass(12);
+         Console.WriteLine(a1.ToString);
+      }
+
+      private void SetUpB() {
+         Console.WriteLine("SetUpB()");
+         BClass b0 = new BClass();
+         BClass b1 = new BClass(150);
+         BClass b2 = new BClass(174, 2);
+         BClass b3 = new BClass(8, 101, 78);
+         Console.WriteLine(b0.ToString);
+         Console.WriteLine(b1.ToString);
+         Console.WriteLine(b2.ToString);
+         Console.WriteLine(b3.ToString);
+      }
+   }
+
+   public class AClass
+   {
+      private int simple0 = 0;
+      private int simple1;
+
+      public AClass()
+      {
+         const int V = -1;
+         this.simple1 = V;
+      }
+
+      public AClass(int value)
+      {
+         this.simple1 = value;
+      }
+
+      public int Simple0() { return simple0; }
+
+      public int Simple1() { return simple1; }
+
+
+      public new String ToString => this.simple0.ToString() + ", " + this.simple1.ToString();
+
+   }
+
+   public class BClass : AClass
+   {
+      private int bsimple  = 0;
+      private int bsimple1 = 23;
+
+      public BClass() {  }
+
+      public BClass(int value)
+         : base(value) //almost similar to the C++ syntax
+      { }
+
+      public BClass(int value0, int value1)
+      : base(value0)
+      {
+         this.bsimple = value1;
+      }
+
+      public BClass(int value0, int value1, int value2)
+      : base(value0)
+      {
+         this.bsimple  = value1;
+         this.bsimple1 = value2;
+      }
+
+      public new String ToString => base.ToString + ", " +
+         this.bsimple.ToString() + ", " + this.bsimple1.ToString();
+   }
+}
---
+++ LouClock/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LouClock
+{
+   class Program
+   {
+      private LTimer lt;
+      private Clock c;
+      static void Main(string[] args)
+      {
+         new Program();
+      }
+
+      public Program()
+      {
+         
+         Console.WriteLine("Hello World");
+         c = new Clock();
+         //lt = new LTimer();
+         //c.AddObserver(lt);
+         //lt.Start();
+
+      }
+   }
+
+   class Clock
+   {
+      private long time = 0;
+      private AutoResetEvent autoEvent;
+      private Timer timer;
+      private DateTime begin;
+      private DateTime current;
+      private ClockNotifier clockNotifier;
+
+      public Clock()
+      {
+         this.begin     = DateTime.Now;
+         this.autoEvent = new AutoResetEvent(false);
+         this.timer     = new Timer(SetTime,autoEvent,0,100);
+         this.clockNotifier = new ClockNotifier();
+         //this.timer     = new Timer(SetTime, null, 0, 100);
+         //this.clockNotifier = new ClockNotifier();
+         this.autoEvent.WaitOne();
+         this.timer.Dispose();
+      }
+
+      public void AddObserver(IClockObserver observer) 
+      {
+         Console.WriteLine(observer);
+         this.clockNotifier.AddObserver(observer); 
+      }
+
+      private void SetTime(Object? stateInfo)
+      {
+         if (stateInfo != null)
+         {
+            AutoResetEvent autoEvent = (AutoResetEvent)stateInfo;
+         }
+         this.current = DateTime.Now;
+         //this.clockNotifier.Trigger();
+         //this.clockNotifier.SetTime(this.current);
+         //most of this needs to eventually be removed
+         if (this.time == 10)
+         {
+            /*
+            Console.Write("Current Time:  ");
+            Console.WriteLine(this.current.ToString("HH:mm:ss.fff"));
+            Console.WriteLine(this.current.ToString());
+            Console.WriteLine("{0:N0}", (this.current.Ticks - this.begin.Ticks));
+            this.begin = this.current;
+            */
+            this.clockNotifier.Trigger();
+            this.clockNotifier.SetTime(this.current);
+            time = 0;
+         }
+         ++time;
+      }
+   }
+
+   class ClockNotifier
+   {
+      private DateTime current;
+      private Timer timer;
+      private Boolean trigger;
+      private List<IClockObserver> observers;
+
+      public ClockNotifier()
+      {
+         const int SLEEP = 10;
+         this.trigger = false;
+         this.observers = new List<IClockObserver>();
+         this.timer = new Timer(SetTime, null, 0, SLEEP);
+      }
+
+      public void AddObserver(IClockObserver observer)
+      {
+         Console.WriteLine("Add the fucking observer");
+         this.observers.Add(observer);
+      }
+
+      public void Trigger() 
+      {
+         this.trigger = true;
+      }
+
+      public void SetTime(DateTime datetime)
+      {
+         if (this.trigger)
+         {
+            this.current = datetime;
+         }
+      }
+
+      private void SetTime(Object? stateInfo)
+      {
+         if(this.trigger && stateInfo == null)
+         {
+            Console.WriteLine(this.current.ToString("HH:mm:ss.fff"));
+            Console.WriteLine(this.current.Ticks);
+            this.trigger = false;
+         }
+      }
+   }
+
+
+   public interface IClockObserver
+   {
+      public void updateTime(DateTime time);
+      public void updateTime(long ticks);
+   }
+
+   class LTimer : IClockObserver
+   {
+      private Boolean run = false;
+      private Boolean receive = false;
+      private DateTime begin;
+      private DateTime current;
+      private TimeSpan elapsed;
+      private long ticks = 0;
+      private long milliseconds = 0;
+      private long seconds = 0;
+      private long minutes = 0;
+      private long hours = 0;
+      private long days = 0;
+
+      /////////////////////////////Constructor(s)/////////////////////
+      public LTimer() { }
+
+      /////////////////////////////Public Methods/////////////////////
+      public void Reset() { }
+
+      public void Start() 
+      {
+         this.SetRun(true);
+         this.SetReceive(true);
+      }
+
+      public void Stop() { }
+      //////////////////////////Protected Methods/////////////////////
+      ////////////////////////////Private Methods/////////////////////
+      private void CalculateTime() { }
+      private void ClearAllTimeValues() { }
+      private void NotifyObservers() { }
+      private void SetRun(Boolean toRun)
+      {
+         this.run = toRun;
+         Console.WriteLine(this.run);
+      }
+
+      private void SetReceive(Boolean toReceive) 
+      {
+         this.receive = toReceive;
+         Console.WriteLine(this.receive);
+      }
+
+      private void SetTime(DateTime time) { }
+
+      private void SetTimeValues() { }
+
+      ///////////////////////////Interface Methods////////////////////
+      public void updateTime(DateTime time) { }
+      public void updateTime(long ticks) { }
+   }
+}
