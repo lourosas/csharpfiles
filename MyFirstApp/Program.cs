cc365b2f464ac351f0ced95f465e2687c5ae4439
Lou Rosas <lourosas@gmail.com>
2023-06-30 17:00:27 -0700

    06-30-23-00

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+internal class NewBaseType
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+public class TestApp{
+   public static void Main(String [] args){
+      // See https://aka.ms/new-console-template for more information
+      Console.WriteLine("Hello, World!");
+   }
+}
+
